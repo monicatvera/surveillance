f273bba50527a17564c141254b4633a92f6d3e69
Mónica Ilenia Tardón Vera <monitvera@gmail.com>
2023-07-07 19:28:48 +0100

    🧪 define the tests

---
+++ src/test/surveillanceController.test.ts
@@ -0,0 +1,15 @@
+describe('The Surveillance Controller', () => {
+
+	it('asks the recorder to stop recording when the sensor detects no motion', () => {
+	});
+
+	it('asks the recorder to start recording when the sensor detects motion', () => {
+	});
+
+	it('asks the recorder to stop recording when the sensor throws an unexpected error', () => {
+
+	});
+
+	it('checks the sensor status once per second', () => {
+	});
+});
